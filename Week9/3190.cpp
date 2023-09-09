be5d6b63aa0fc9ce3c296b7bf913775a36f6ad98
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2023-09-09 14:28:56 +0900

    Update 3190.cpp

--- Week9/3190.cpp
+++ Week9/3190.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int N, K, L;
 int board[100][100] = { 0 };
-pair<pair<int, int>, pair<int, int>> sneak = { {0,0},{0,0} }; // 뱀 머리 좌표, 뱀 꼬리 좌표
+pair<pair<int, int>, pair<int, int>> snake = { {0,0},{0,0} }; // 뱀 머리 좌표, 뱀 꼬리 좌표
 vector<pair<int, char>> command;
-int sneak_length = 0;
+int snake_length = 0;
 
 void change_dir(int& dir, char c) {
 	if (c == 'L') { // 상->좌 하->우 좌->하 우->상
@@ -28,21 +28,22 @@ void change_dir(int& dir, char c) {
 }
 
 void tail(int idx, int dir, int cnt) {
-	board[sneak.second.first][sneak.second.second] = 0;
+	board[snake.second.first][snake.second.second] = 0;
 	if (idx == 0) // 아직 진행 방향이 바뀌기 전이기 때문에 꼬리도 무조건 오른쪽 방향으로 움직임
-		sneak.second.second++;
+		snake.second.second++;
 	else { // 꼬리와 머리의 진행 방향이 다를 수 있음, 꼬리는 항상 머리보다 총 길이만큼 늦게 방향을 바꿈
-		if (cnt >= (command[idx - 1].first + sneak_length)) { // 머리가 방향을 바꾸고 총 길이만큼 시간이 더 지난 경우
+		if (cnt >= (command[idx - 1].first + snake_length)) { // 머리가 방향을 바꾸고 총 길이만큼 시간이 더 지난 경우
 			// 꼬리와 머리의 진행 방향이 같음
 			if (dir == 0) // 상
-				sneak.second.first--;
+				snake.second.first--;
 			else if (dir == 1) // 하
-				sneak.second.first++;
+				snake.second.first++;
 			else if (dir == 2) // 좌
-				sneak.second.second--;
+				snake.second.second--;
 			else if (dir == 3) // 우
-				sneak.second.second++;
+				snake.second.second++;
 		}
+
 		else { // 총 길이만큼 시간이 더 지나지 않아 꼬리와 머리의 진행 방향이 다른 경우
 			char c = (command[idx - 1].second == 'L') ? 'D' : 'L'; // 이전 진행 방향으로 바꾸기 위함
 			change_dir(dir, c); // 이전 진행 방향으로 바꾼 후
@@ -51,22 +52,23 @@ void tail(int idx, int dir, int cnt) {
 	}
 }
 
-bool move_sneak(int idx, int dir, int& cnt) {
-    int x = command[idx].first;
-    if (idx == -1){
-        x = cnt + N;
-        idx = L;
-    }
+bool move_snake(int idx, int dir, int& cnt) {
+	int x = command[idx].first;
+	
+	if (idx == -1) {
+		x = cnt + N;
+		idx = L;
+	}
 
 	while (cnt < x) {
 		if (dir == 0) { // 상
-			if (sneak.first.first - 1 >= 0) {
-				if (board[sneak.first.first - 1][sneak.first.second] != 1) {
-					if (board[sneak.first.first - 1][sneak.first.second] == 2) // 사과를 만나면
-						sneak_length++;
+			if (snake.first.first - 1 >= 0) {
+				if (board[snake.first.first - 1][snake.first.second] != 1) {
+					if (board[snake.first.first - 1][snake.first.second] == 2) // 사과를 만나면
+						snake_length++;
 					else // 사과가 아니면
 						tail(idx, dir, cnt); // 꼬리 위치를 0으로 바꾸고 꼬리 좌표 갱신
-					board[--sneak.first.first][sneak.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
+					board[--snake.first.first][snake.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
 				}
 				else { // 자기 몸에 부딪힌 경우
 					cnt++;
@@ -81,13 +83,13 @@ bool move_sneak(int idx, int dir, int& cnt) {
 		}
 
 		else if (dir == 1) { // 하
-			if (sneak.first.first + 1 < N) {
-				if (board[sneak.first.first + 1][sneak.first.second] != 1) {
-					if (board[sneak.first.first + 1][sneak.first.second] == 2) // 사과를 만나면
-						sneak_length++;
+			if (snake.first.first + 1 < N) {
+				if (board[snake.first.first + 1][snake.first.second] != 1) {
+					if (board[snake.first.first + 1][snake.first.second] == 2) // 사과를 만나면
+						snake_length++;
 					else // 사과가 아니면
 						tail(idx, dir, cnt); // 꼬리 위치를 0으로 바꾸고 꼬리 좌표 갱신
-					board[++sneak.first.first][sneak.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
+					board[++snake.first.first][snake.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
 				}
 				else { // 자기 몸에 부딪힌 경우
 					cnt++;
@@ -102,13 +104,13 @@ bool move_sneak(int idx, int dir, int& cnt) {
 		}
 
 		else if (dir == 2) { // 좌
-			if (sneak.first.second - 1 >= 0) {
-				if (board[sneak.first.first][sneak.first.second - 1] != 1) {
-					if (board[sneak.first.first][sneak.first.second - 1] == 2) // 사과를 만나면
-						sneak_length++;
+			if (snake.first.second - 1 >= 0) {
+				if (board[snake.first.first][snake.first.second - 1] != 1) {
+					if (board[snake.first.first][snake.first.second - 1] == 2) // 사과를 만나면
+						snake_length++;
 					else // 사과가 아니면
 						tail(idx, dir, cnt); // 꼬리 위치를 0으로 바꾸고 꼬리 좌표 갱신
-					board[sneak.first.first][--sneak.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
+					board[snake.first.first][--snake.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
 				}
 				else { // 자기 몸에 부딪힌 경우
 					cnt++;
@@ -123,13 +125,13 @@ bool move_sneak(int idx, int dir, int& cnt) {
 		}
 
 		else if (dir == 3) { // 우
-			if (sneak.first.second + 1 < N) {
-				if (board[sneak.first.first][sneak.first.second + 1] != 1) {
-					if (board[sneak.first.first][sneak.first.second + 1] == 2) // 사과를 만나면
-						sneak_length++;
+			if (snake.first.second + 1 < N) {
+				if (board[snake.first.first][snake.first.second + 1] != 1) {
+					if (board[snake.first.first][snake.first.second + 1] == 2) // 사과를 만나면
+						snake_length++;
 					else // 사과가 아니면
 						tail(idx, dir, cnt); // 꼬리 위치를 0으로 바꾸고 꼬리 좌표 갱신
-					board[sneak.first.first][++sneak.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
+					board[snake.first.first][++snake.first.second] = 1; // 머리 좌표 갱신 후 머리 위치를 1로 바꿈
 				}
 				else { // 자기 몸에 부딪힌 경우
 					cnt++;
@@ -158,7 +160,7 @@ int main()
 	for (int i = 0; i < K; i++) {
 		int x, y;
 		cin >> x >> y;
-		board[x - 1][y - 1] = 2; 
+		board[x - 1][y - 1] = 2;
 	}
 	cin >> L;
 	for (int i = 0; i < L; i++) {
@@ -172,17 +174,17 @@ int main()
 	int cnt = 0;
 	bool isbool;
 	for (int i = 0; i < L; i++) {
-		isbool = move_sneak(i, dir, cnt);
+		isbool = move_snake(i, dir, cnt);
 		change_dir(dir, command[i].second);
 		if (!isbool)
 			break;
 	}
 
 	while (isbool) {
-		isbool = move_sneak(-1, dir, cnt);
+		isbool = move_snake(-1, dir, cnt);
 	}
 
 	cout << cnt << "\n";
-	
+
 	return 0;
 }
