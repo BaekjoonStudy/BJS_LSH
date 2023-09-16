ec0640c59e59b99b71e3b32ee0a0497a7b1cdbf4
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2023-09-16 17:07:15 +0900

    Update 14502.cpp

--- Week10/14502.cpp
+++ Week10/14502.cpp
@@ -30,7 +30,7 @@ int spread_virus() { // bfs
 		for (int i = 0; i < 4; i++) { // 상하좌우
 			int row_tmp = row + dir[i][0];
 			int col_tmp = col + dir[i][1];
-			
+
 			if (row_tmp >= 0 && row_tmp < N && col_tmp >= 0 && col_tmp < M) { // map을 벗어나지 않고
 				if (map[row_tmp][col_tmp] == 0 && visited[row_tmp][col_tmp] == 0) { // 해당 좌표가 안전 영역이며 방문한적 없다면
 					q.push({ row_tmp, col_tmp }); // 바이러스로
@@ -39,7 +39,7 @@ int spread_virus() { // bfs
 					//cout << i << " : " << row_tmp << "," << col_tmp << " " << c << "\n";
 				}
 			}
-			
+
 		}
 	}
 	return c; // 원래 안전 영역이었다가 벽 또는 바이러스가 퍼진 영역의 수
@@ -64,8 +64,8 @@ int main()
 	initialize(); // queue, visited 배열 초기화
 	// 방문했다고 표시함으로써 바이러스가 퍼지지 못하게함
 	for (int i = 0; i < safe_area.size() - 2; i++) {
-		for (int j = 1; j < safe_area.size() - 1; j++) {
-			for (int k = 2; k < safe_area.size(); k++) {
+		for (int j = i + 1; j < safe_area.size() - 1; j++) {
+			for (int k = j + 1; k < safe_area.size(); k++) {
 				visited[safe_area[i].first][safe_area[i].second] = 1;
 				visited[safe_area[j].first][safe_area[j].second] = 1;
 				visited[safe_area[k].first][safe_area[k].second] = 1;
@@ -76,7 +76,7 @@ int main()
 			}
 		}
 	}
-	
+
 	cout << result << "\n";
 
 	return 0;
