d0558e30459f8f5f2e7fc0d0aee74f33d748c98c
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2023-12-02 02:54:44 +0900

    Create 2022_kakao_blind_사라지는발판.cpp

---
+++ Week16/2022_kakao_blind_사라지는발판.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
+    int answer = -1;
+    
+    return answer;
+}
