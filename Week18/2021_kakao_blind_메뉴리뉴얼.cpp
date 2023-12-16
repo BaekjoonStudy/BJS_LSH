8d181ff23199c6390e4d91073c945d6d27f09f43
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2023-12-16 15:11:28 +0900

    Update 2021_kakao_blind_메뉴리뉴얼.cpp

--- Week18/2021_kakao_blind_메뉴리뉴얼.cpp
+++ Week18/2021_kakao_blind_메뉴리뉴얼.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 vector<string> c_size[11]; // 코스 수에 따라 정리, ex) c_size[2] = {"AB", "AC", "BD"} >> "AB","AC","BD"가 2개로 구성된 코스 중 가장 많이 나온 코스라는 의미
 map<string, int> c_count; // 각 코스별 횟수 카운팅, ex) c_count["AB"] = 3 >> "AB"가 세 번 나왔다는 의미
-int max_count[11]; // max_count[10] = 5 >> 코스 수가 10개인 코스 중 가장 많이 나온 코스는 5번 나온 코스라는 의미
+int max_count[11]; // max_count[2] = 3 >> 코스 수가 2개인 코스 중 가장 많이 나온 코스는 3번 나온 코스라는 의미
 
 bool comp(string a, string b) {
     int isbool = (a.size() <= b.size()) ? 0 : 1;
@@ -32,7 +32,7 @@ bool comp(string a, string b) {
     }
 }
 
-void dfs(string cur, vector<int> visit, string order, int i, int idx) {
+void dfs(string cur, string order, int i, int idx) {
     if (cur.size() == i) {
         c_count.insert({cur,0});
         c_count[cur]++;
@@ -47,19 +47,14 @@ void dfs(string cur, vector<int> visit, string order, int i, int idx) {
     }
     
     for (int j = idx; j < order.size(); j++) { // AB와 BA처럼 중복되는 경우를 방지하기 위함
-        if (visit[j] == 0) { 
-            visit[j] = 1;
-            dfs(cur+order[j], visit, order, i, j);
-            visit[j] = 0;
-        }
+        dfs(cur+order[j], order, i, j + 1);
     }
 }
 
 
 void func(string order, vector<int> course) {
     for (auto i : course) {
-        vector<int> visit(order.size(), 0);
-        dfs("", visit, order, i, 0);
+        dfs("", order, i, 0);
     }
 }
 
